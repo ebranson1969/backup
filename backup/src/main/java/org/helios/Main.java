2726925f8e27e70a357a49b199cfbc34d888b615
ebranson1969 <ebranson1969@gmail.com>
2017-08-28 09:19:20 -0500

    new

---
+++ backup/src/main/java/org/helios/Main.java
@@ -0,0 +1,44 @@
+package org.helios;
+
+import org.helios.BackupConfiguration;
+import org.helios.BackupJSON;
+
+import java.io.File;
+
+/**
+ * Created by ebranson1969 on 08/27/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            BackupJSON backupJSON = new BackupJSON();
+            BackupConfiguration backupConfiguration = backupJSON.getBackupConfiguration();
+            backupJSON.SaveBackupConfiguration();
+
+            String lastBackup = backupConfiguration.AddStartBackup();
+            backupJSON.SaveBackupConfiguration();
+            if(lastBackup != null)
+                System.out.println("Backup occured: " + lastBackup);
+
+            int count = 0;
+            while (true) {
+                lastBackup = backupConfiguration.AddBackup();
+                backupJSON.SaveBackupConfiguration();
+                if(lastBackup != null)
+                    System.out.println("Backup occured: " + lastBackup);
+
+                //Thread.sleep(backupConfiguration.GetSleep() - 1000);
+
+                File kill = new File("kill");
+                if(kill.exists()) {
+                    kill.delete();
+                    break;
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.toString());
+        }
+    }
+}
